abaff603e216576d62128dc30fba2df9f2328474
libertador92 <miralizadenicat@gmail.com>
2024-06-21 06:51:53 -0400

    js homework4

---
+++ app.js
@@ -0,0 +1,8 @@
+
+let num = prompt("4 reqemli eded siyahiya daxil edin.");
+console.log(+num[0] + +num[1] + +num[2] + +num[3]);
+
+
+let name$surname = "Nicat Miralizade";
+let fullName = prompt("Zehmet olmasa adinizi ve soyadinizi daxil edin.");
+console.log("Salam" + " " + name$surname.split(" ")[0]);
